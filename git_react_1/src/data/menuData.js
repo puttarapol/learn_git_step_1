f0eb71d3cc852b269f1bc9b48af069bc2b2463c7
valo-67 <valothanawat67@gmail.com>
2025-06-13 11:37:46 +0700

    menuItem

---
+++ git_react_1/src/data/menuData.js
@@ -0,0 +1,16 @@
+export const menuIteems =[{
+
+    id:1,
+    name:"ผัดพริกแกงหมูกรอบ",
+    price:50,
+    Image: "https://img.wongnai.com/p/1968x0/2019/07/02/27ebe422d9314a238932913717ead1db.jpg",
+    desc:"หมูกรอบ กัด ฟัน หัก"
+},
+{
+    
+     id:2,
+    name:"ผัดตับ",
+    price:70,
+    Image:"https://img.wongnai.com/p/1968x0/2019/08/28/9fcef44d93604033af8201b3aea4e440.jpg ",
+    desc:"ตับ ตับ ตับ"
+    }]
